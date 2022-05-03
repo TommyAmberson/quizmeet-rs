8a4e6a2c981d21f29f917516bf705a1614ea33bc
Tommy Amberson <53712733+TommyAmberson@users.noreply.github.com>
2022-05-03 00:25:41 -0600

    add binary for debugging

---
+++ src/bin/main.rs
@@ -0,0 +1,14 @@
+use quizmeet_rs::parse;
+use std::path::Path;
+
+fn main() {
+    let result = parse::read_from_file(Path::new("ods/D1Q1.ods"));
+    match result {
+        Ok(result) => {
+            dbg!(result);
+        },
+        Err(e) => {
+            dbg!(e);
+        },
+    }
+}
