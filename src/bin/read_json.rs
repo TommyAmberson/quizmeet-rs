70d12800a52efc92dd0275fb152cb4a2cf1f4ed6
Tommy Amberson <53712733+TommyAmberson@users.noreply.github.com>
2022-05-04 01:06:47 -0600

    add read to io.rs and make read_json bin

--- src/bin/read_json.rs
+++ src/bin/read_json.rs
@@ -1,10 +1,5 @@
-extern crate serde_json;
-
 use clap::Parser;
-use glob::glob;
-use quizmeet_rs::{entries::*, parse};
-use std::fs::File;
-use std::path::{Path, PathBuf};
+use quizmeet_rs::{entries::*, io::*};
 
 #[derive(Parser)]
 struct Cli {
@@ -13,36 +8,17 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let g = args.g.unwrap_or(String::from("ods/*.ods"));
-    match from_glob(&g) {
-        Ok(_result) => {
-            // dbg!(result);
-        }
-        Err(e) => {
-            dbg!(e);
-        }
-    }
-}
-
-pub fn from_glob(g: &str) -> Result<(), Box<dyn std::error::Error>> {
-    for entry in glob(g)?.filter_map(Result::ok) {
-        let mut out = PathBuf::new();
-        out.push("json");
-        out.push(entry.file_name().unwrap());
-        out.set_extension("json");
-        translate(entry.as_path(), out.as_path())?;
-    }
-    Ok(())
-}
-
-fn translate(
-    read: &Path,
-    write: &Path,
-) -> Result<(Vec<TeamEntry>, Vec<QuizzerEntry>), Box<dyn std::error::Error>> {
-    // let mut path = PathBuf::from(p);
-    dbg!(&read);
-    let result = parse::read_from_file(read)?;
-    dbg!(&write);
-    ::serde_json::to_writer(&File::create(write)?, &result)?;
-    Ok(result)
+    let g = args.g.unwrap_or(String::from("json/*.json"));
+    let mut team_entries: Vec<TeamEntry> = Vec::new();
+    let mut quizzer_entries: Vec<QuizzerEntry> = Vec::new();
+    let r = from_glob(&g, |entry| {
+        let result = read(entry.as_path())?;
+        // dbg!(&result);
+        team_entries.extend(result.0);
+        quizzer_entries.extend(result.1);
+        Ok(())
+    });
+    dbg!(&r);
+    r.unwrap();
+    // println!("team_entries: {:?}\nquizzer_entries: {:?}", team_entries, quizzer_entries);
 }
--- src/io.rs
+++ src/io.rs
@@ -6,11 +6,12 @@ use glob::glob;
 use std::fs::File;
 use std::path::{Path, PathBuf};
 
-pub fn from_glob<F>(g: &str, action: F) -> Result<(), Box<dyn std::error::Error>>
+pub fn from_glob<F>(g: &str, mut action: F) -> Result<(), Box<dyn std::error::Error>>
 where
-    F: Fn(PathBuf) -> Result<(), Box<dyn std::error::Error>>,
+    F: FnMut(PathBuf) -> Result<(), Box<dyn std::error::Error>>,
 {
     for entry in glob(g)?.filter_map(Result::ok) {
+        dbg!(&entry);
         action(entry)?;
     }
     Ok(())
@@ -24,10 +25,18 @@ pub fn translate(entry: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
     write(parse::read_from_file(entry.as_path())?, out.as_path())
 }
 
-fn write(
+pub fn write(
     result: (Vec<TeamEntry>, Vec<QuizzerEntry>),
     file: &Path,
 ) -> Result<(), Box<dyn std::error::Error>> {
     serde_json::to_writer(&File::create(file)?, &result)?;
     Ok(())
+}
+
+pub fn read(
+    file: &Path,
+) -> Result<(Vec<TeamEntry>, Vec<QuizzerEntry>), Box<dyn std::error::Error>> {
+    // let json: (Vec<TeamEntry>, Vec<QuizzerEntry>) = serde_json::from_reader(&File::open(file)?)?;
+    let json = serde_json::from_reader(&File::open(file)?)?;
+    Ok(json)
 }
