c41fff3943ddc9dd4237b0d5ad2ef4eaa2cb9fbd
Tommy Amberson <53712733+TommyAmberson@users.noreply.github.com>
2022-05-03 18:58:24 -0600

    update bin main to read from command line

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,22 @@
 extern crate serde_json;
 
-use std::fs::File;
-
+use clap::Parser;
+use glob::glob;
 use quizmeet_rs::{entries::QuizEntry, parse};
-use std::path::Path;
+use std::fs::File;
+use std::path::{Path,PathBuf};
+
+#[derive(Parser)]
+struct Cli {
+    g: Option<String>,
+}
 
 fn main() {
-    match write() {
-        Ok(result) => {
-            dbg!(result);
+    let args = Cli::parse();
+    let g = args.g.unwrap_or(String::from("ods/*.ods"));
+    match from_glob(&g) {
+        Ok(_result) => {
+            // dbg!(result);
         }
         Err(e) => {
             dbg!(e);
@@ -16,8 +24,22 @@ fn main() {
     }
 }
 
-fn write() -> Result<QuizEntry, Box<dyn std::error::Error>> {
-    let result = parse::read_from_file(Path::new("ods/D1Q1.ods"))?;
-    ::serde_json::to_writer(&File::create("data.json")?, &result)?;
+pub fn from_glob(g: &str) -> Result<(), Box<dyn std::error::Error>> {
+    for entry in glob(g)?.filter_map(Result::ok) {
+        let mut out = PathBuf::new();
+        out.push("json");
+        out.push(entry.file_name().unwrap());
+        out.set_extension("json");
+        translate(entry.as_path(), out.as_path())?;
+    }
+    Ok(())
+}
+
+fn translate(read: &Path, write: &Path) -> Result<QuizEntry, Box<dyn std::error::Error>> {
+    // let mut path = PathBuf::from(p);
+    dbg!(&read);
+    let result = parse::read_from_file(read)?;
+    dbg!(&write);
+    ::serde_json::to_writer(&File::create(write)?, &result)?;
     Ok(result)
 }
