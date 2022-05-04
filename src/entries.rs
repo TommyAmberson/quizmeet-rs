9003a89fd0fd0411daaf84ddf25d1f165fa70019
Tommy Amberson <53712733+TommyAmberson@users.noreply.github.com>
2022-05-03 22:08:18 -0600

    add fn group by name
    this fn is to convert a vec into a hashmap of vecs where each vec only
    has entries with the same name.

--- src/entries.rs
+++ src/entries.rs
@@ -1,4 +1,9 @@
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+pub trait Entry {
+    fn get_name(&self) -> &str;
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TeamEntry {
@@ -8,6 +13,12 @@ pub struct TeamEntry {
     pub score: i32,
     pub points: i32,
     pub errors: i32,
+}
+
+impl Entry for TeamEntry {
+    fn get_name(&self) -> &str {
+        &self.name
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -24,4 +35,29 @@ pub struct QuizzerEntry {
     pub ma: i32,
     pub q: i32,
     pub sit: i32,
+}
+
+impl Entry for QuizzerEntry {
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+}
+
+pub fn group_by_name<T>(entries: Vec<T>) -> HashMap<String, Vec<T>>
+where
+    T: Entry,
+{
+    let mut map: HashMap<String, Vec<T>> = HashMap::new();
+    for entry in entries {
+        let name = entry.get_name();
+        if !map.contains_key(name) {
+            map.insert(String::from(name), Vec::new());
+        }
+        map.get_mut(name).unwrap().push(entry);
+        // match map.get_mut(name) {
+        //     Some(v) => v.push(entry),
+        //     _ => (),
+        // }
+    }
+    map
 }
