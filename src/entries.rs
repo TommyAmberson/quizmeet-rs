f40f87fc5f26c7104ce102df55b0820a3228c47a
Tommy Amberson <53712733+TommyAmberson@users.noreply.github.com>
2022-05-03 23:10:58 -0600

    add emtpy method for Entry trait to get an empty entry

--- src/entries.rs
+++ src/entries.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 
 pub trait Entry {
     fn get_name(&self) -> &str;
+    fn empty() -> Self;
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -18,6 +19,16 @@ pub struct TeamEntry {
 impl Entry for TeamEntry {
     fn get_name(&self) -> &str {
         &self.name
+    }
+    fn empty() -> Self {
+        Self {
+            name: String::from(""),
+            quiz: String::from(""),
+            place: 0.,
+            score: 0,
+            points: 0,
+            errors: 0,
+        }
     }
 }
 
@@ -40,6 +51,22 @@ pub struct QuizzerEntry {
 impl Entry for QuizzerEntry {
     fn get_name(&self) -> &str {
         &self.name
+    }
+    fn empty() -> Self {
+        Self {
+            name: String::from(""),
+            team: String::from(""),
+            quiz: String::from(""),
+            points: 0,
+            errors: 0,
+            jumps: 0,
+            refer: 0,
+            ftv: 0,
+            int: 0,
+            ma: 0,
+            q: 0,
+            sit: 0,
+        }
     }
 }
 
