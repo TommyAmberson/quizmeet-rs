34363739d013c497a7e855d24c9aec43c48c5e18
Tommy Amberson <53712733+TommyAmberson@users.noreply.github.com>
2022-05-05 16:25:29 -0600

    formatting

--- src/bin/globtest.rs
+++ src/bin/globtest.rs
@@ -14,8 +14,12 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
     let g = args.glob.unwrap_or(String::from("json/*.json"));
-    // let g = String::from("json/*.json");
-    let re = Regex::new(&args.regex.unwrap_or(String::from(r"^json/D(?P<d>\dC?)Q(?P<q>(\d|\w)+).json$"))).unwrap();
+    let re = Regex::new(
+        &args
+            .regex
+            .unwrap_or(String::from(r"^json/D(?P<d>\dC?)Q(?P<q>(\d|\w)+).json$")),
+    )
+    .unwrap();
     dbg!(&re);
     from_glob(&g, |e| {
         if re.is_match(e.to_str().unwrap()) {
