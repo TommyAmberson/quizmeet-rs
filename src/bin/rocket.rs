9e1be0d11d72b324e513a385b2f35795e26ff7c6
Tommy Amberson <53712733+TommyAmberson@users.noreply.github.com>
2022-05-06 01:00:38 -0600

    mock up some js

--- src/bin/rocket.rs
+++ src/bin/rocket.rs
@@ -9,10 +9,10 @@ use quizmeet_rs::{entries::Entry, quiz_sum::*, stats::*};
 use regex::Regex;
 use rocket_dyn_templates::Template;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+// #[get("/")]
+// fn index() -> &'static str {
+//     "Hello, world!"
+// }
 
 #[get("/summary")]
 fn summary() -> String {
@@ -42,7 +42,7 @@ fn parse() -> String {
     )
 }
 
-fn table_template(regex: Option<Regex>) -> Template {
+fn table_template(regex: Option<Regex>, plain: bool) -> Template {
     let (team_sums, quizzer_sums) = get_lists(Some(String::from("json/*.json")), regex).unwrap();
     let team_avgs: Vec<String> = team_sums
         .iter()
@@ -54,7 +54,7 @@ fn table_template(regex: Option<Regex>) -> Template {
         .collect();
 
     Template::render(
-        "table-view",
+        if plain { "table" } else { "table-view" },
         context! {
             team_sums,
             team_avgs,
@@ -64,9 +64,14 @@ fn table_template(regex: Option<Regex>) -> Template {
     )
 }
 
-#[get("/table")]
+#[get("/")]
 pub fn table() -> Template {
-    table_template(None)
+    table_template(None, false)
+}
+
+#[get("/table")]
+pub fn table_plain() -> Template {
+    table_template(None, true)
 }
 
 #[get("/table/div/<div>")]
@@ -74,14 +79,21 @@ pub fn table_div(div: &str) -> Template {
     let mut r = String::from("D");
     r += div;
     r += r"Q(?P<q>(\d|\w)+).json$";
-    table_template(Some(Regex::new(r.as_str()).unwrap()))
+    table_template(Some(Regex::new(r.as_str()).unwrap()), true)
 }
 
-#[get("/table/<regex>")]
+#[get("/<regex>")]
 pub fn table_regex(regex: &str) -> Template {
     let iter = percent_decode(regex.as_bytes());
     let decoded = iter.decode_utf8_lossy().into_owned();
-    table_template(Some(Regex::new(&decoded).unwrap()))
+    table_template(Some(Regex::new(&decoded).unwrap()), false)
+}
+
+#[get("/table/<regex>")]
+pub fn table_regex_plain(regex: &str) -> Template {
+    let iter = percent_decode(regex.as_bytes());
+    let decoded = iter.decode_utf8_lossy().into_owned();
+    table_template(Some(Regex::new(&decoded).unwrap()), true)
 }
 
 #[get("/tera")]
@@ -100,6 +112,9 @@ pub fn tera() -> Template {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, summary, parse, table, table_div, table_regex, tera])
+        .mount(
+            "/",
+            routes![summary, parse, table, table_plain, table_div, table_regex, table_regex_plain, tera],
+        )
         .attach(Template::fairing())
 }
