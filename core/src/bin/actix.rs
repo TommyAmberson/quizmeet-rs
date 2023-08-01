e47c91133b169f93662ce506279c8301f9b6e424
Tom Amberson <tom.amberson@ravenind.com>
2023-08-01 11:20:40 -0600

    Revert "WIP remove actix"
    This reverts commit 360a61b45d0c79393104606efbd49e39d2742df5.

---
+++ core/src/bin/actix.rs
@@ -0,0 +1,10 @@
+use actix_web::{App, HttpServer};
+use quizmeet_rs_actix::hello;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
